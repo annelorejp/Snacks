2d546ce5fe55716b1d605f9bdd24cbeb7499abcc
Verena Schröppel <verena.schroeppel@outlook.de>
2019-12-02 14:33:03 +0100

    unzipped the project and made everthing git ready

--- Rapid/js/main.js
+++ Rapid/js/main.js
@@ -100,11 +100,11 @@
 
   // Porfolio isotope and filter
   $(window).on('load', function () {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
+    var portfolioIsotope = $('.sales-container').isotope({
+      itemSelector: '.sales-item'
     });
-    $('#portfolio-flters li').on( 'click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
+    $('#sales-flters li').on( 'click', function() {
+      $("#sales-flters li").removeClass('filter-active');
       $(this).addClass('filter-active');
   
       portfolioIsotope.isotope({ filter: $(this).data('filter') });
